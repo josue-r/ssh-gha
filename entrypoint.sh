148bc40d8f204cfbf0361eff44b58a771da3b334
Josue-RS <josue.ruiz@wizeline.com>
2024-04-09 20:07:12 -0600

    FIX: add files for ssh connection

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Fail if any commands fails
+set -e
+# Echo the commands being executed
+set -x
+
+# Connect to the remote server and execute a command
+sshpass -p $INPUT_PASSWORD ssh -o StrictHostKeyChecking=no -p $INPUT_PORT $INPUT_USERNAME@$INPUT_HOST "$INPUT_SCRIPT"
